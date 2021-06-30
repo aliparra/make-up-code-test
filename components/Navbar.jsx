aaa15a0b40ad36b863298550008cca9c90309511
aliparra <alicia@redparra.com>
2021-06-30 18:50:31 +0200

    Add boostrap. Add Navbar and Footer. Add products route

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+
+
+const Navbar = () => {
+    return (
+        <div className="Navbar">
+            <nav className="navbar navbar-light bg-light py-3">
+            <div className="container-fluid">
+            <a className="navbar-brand px-4 py-2 d-flex align-items-center" href="/">
+                <img src="../img/cepillo.png" alt=""  height="40" />
+                <h2 className='mx-4'>Make-Up-Test</h2>
+            </a>
+            </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Navbar;
