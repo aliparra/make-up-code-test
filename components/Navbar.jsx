b8b37b3a659503b0ddb4167095cdbd114d49b8ae
aliparra <alicia@redparra.com>
2021-06-30 18:58:05 +0200

    Add Navbar styles

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,13 @@
-
+import styles from '../styles/Navbar.module.css'
 
 const Navbar = () => {
     return (
         <div className="Navbar">
-            <nav className="navbar navbar-light bg-light py-3">
+            <nav className={`${styles.Navbar} navbar navbar-light py-3 `}>
             <div className="container-fluid">
             <a className="navbar-brand px-4 py-2 d-flex align-items-center" href="/">
                 <img src="../img/cepillo.png" alt=""  height="40" />
-                <h2 className='mx-4'>Make-Up-Test</h2>
+                <h2 className={`mx-4 ${styles.light__theme}`}>Make-Up-Test</h2>
             </a>
             </div>
             </nav>
